77aba79200373c4c99519f26d5f7096b77a170ae
kortexcode <darkmaeno323@gmail.com>
2023-08-10 11:44:55 -0500

    Se cambia estilos del foco de los botones del carrusel, se agrega tabIndex a los proyectos del carrusel.

--- index.js
+++ index.js
@@ -21,7 +21,20 @@ function clickRight() {
     return;
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
+  //Variando el tabIndex de la navegación en cada proyecto
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch (newValue) {
+    case -270:
+      document.querySelector(".project1").setAttribute("tabindex", "-1");
+      document.querySelector(".project4").removeAttribute("tabindex");
+      break;
+    case -540:
+      document.querySelector(".project2").setAttribute("tabindex", "-1");
+      document.querySelector(".project5").removeAttribute("tabindex");
+      break;
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
@@ -36,6 +49,19 @@ function clickLeft() {
   }
   let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
+  //Variando el tabIndex de la navegación en cada proyecto
+  switch (newValue) {
+    case -270:
+      document.querySelector(".project5").setAttribute("tabindex", "-1");
+      document.querySelector(".project2").removeAttribute("tabindex");
+      break;
+    case 0:
+      document.querySelector(".project4").setAttribute("tabindex", "-1");
+      document.querySelector(".project1").removeAttribute("tabindex");
+      break;
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
